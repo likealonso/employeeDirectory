3b7a3e73d2e33b7658be7fd3a2ea321d6770e72a
Tacanga <juan.tacanga@accenture.com>
2022-03-15 22:58:39 -0700

    first commit

---
+++ src/main/java/com/luv2code/springboot/cruddemo/controllers/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.luv2code.springboot.cruddemo.controllers;
+
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    private EmployeeRepository emp;
+
+    @GetMapping("/allEmployees")
+    public String showEmployees(Model model) {
+        model.addAttribute("allEmployees", emp.findAllByOrderByLastNameAsc());
+        return "employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model){
+        Optional<Employee> empl = emp.findById(id);
+        model.addAttribute("employee", empl);
+        return "/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        Employee empl = emp.saveAndFlush(employee);
+        return "redirect:/allEmployees";
+
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id){
+        emp.deleteById(id);
+        return "redirect:/allEmployees";
+    }
+}
---
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    public List<Employee> findAllByOrderByLastNameAsc();
+}
